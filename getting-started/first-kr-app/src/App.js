063e88a152dd558579ea26cde9734dc02a9d01b8
shermannatrix <shermannatrix@gmail.com>
2024-03-20 16:26:07 +0800

    Tried my first KendoReact control

--- getting-started/first-kr-app/src/App.js
+++ getting-started/first-kr-app/src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import { Button } from '@progress/kendo-react-buttons';
-import kendoka from './kendoka.svg';
+import { Calendar } from '@progress/kendo-react-dateinputs';
 import './App.css';
 
 function App() {
-  const handleClick = React.useCallback(() => {
-    window.open('https://www.telerik.com/kendo-react-ui/components/', '_blank');
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={kendoka} className="App-logo" alt="kendoka" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button
-          themeColor={'primary'}
-          size={"large"}
-          onClick={handleClick}
-        >
-          Learn KendoReact
-        </Button>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello KendoReact!</h1>
+			<Calendar />
+		</div>
+	);
 }
 
 export default App;
