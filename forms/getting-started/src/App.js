12f3f1d293c6f2b587d96ea952a7d6185a7dc849
shermannatrix <shermannatrix@gmail.com>
2024-04-18 19:52:04 +0800

    Doing my own deep-dive into Forms.

---
+++ forms/getting-started/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Calendar } from '@progress/kendo-react-dateinputs';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello KendoReact!</h1>
+			<Calendar />
+		</div>
+	);
+}
+
+export default App;
