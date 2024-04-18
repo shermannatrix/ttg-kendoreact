db53093d34bcd098146d5f98413beed28116fd98
Shermannatrix <shermannatrix@gmail.com>
2024-04-19 00:17:18 +0800

    Done with the forms and labels demo example.

--- forms/deep-dive/src/App.js
+++ forms/deep-dive/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import '@progress/kendo-theme-material/dist/all.css';
-import SimpleForm from './components/simple-form';
+// import SimpleForm from './components/simple-form';
 import './App.css';
+import FormsAndLabels from './components/forms-labels';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Getting Started with the Form Component in KendoReact!</h1>
-			<SimpleForm />
+			<h1>Deep-diving into the Form Component in KendoReact!</h1>
+			{/* <SimpleForm /> */}
+			<FormsAndLabels />
 		</div>
 	);
 }
