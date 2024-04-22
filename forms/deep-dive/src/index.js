91e02bd2937b456e7f992272fdad53f34db4d9ec
shermannatrix <shermannatrix@gmail.com>
2024-04-22 13:22:58 +0800

    Finally understand where to import the themes/styles.

--- forms/deep-dive/src/index.js
+++ forms/deep-dive/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import './material-lime-dark.css';
+import './styles/material-themes/material-arctic.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
