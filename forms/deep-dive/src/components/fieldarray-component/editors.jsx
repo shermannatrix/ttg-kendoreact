f68b686a111e7d2d494b58e7150fdbbb18022b8c
Shermannatrix <shermannatrix@gmail.com>
2024-04-24 01:09:40 +0800

    Still working through the fieldarray example

--- forms/deep-dive/src/components/fieldarray-component/editors.jsx
+++ forms/deep-dive/src/components/fieldarray-component/editors.jsx
@@ -5,3 +5,60 @@ import { Error } from '@progress/kendo-react-labels';
 import { FormGridEditContext } from '.';
 const FORM_DATA_INDEX = "formDataIndex";
 const requiredValidator = (value) => (value ? "" : "The field is required");
+const DisplayValue = (fieldRenderProps) => {
+  return <>{fieldRenderProps.value}</>;
+};
+
+const TextInputWithValidation = (fieldRenderProps) => {
+  const { validationMessage, visited, ...others } = fieldRenderProps;
+
+  return (
+    <div>
+      <Input {...others} />
+      {visited && validationMessage && <Error>{validationMessage}</Error>}
+    </div>
+  );
+};
+
+const minValidator = (value) => (value >= 0 ? "" : "Minimum units 0");
+const NumericTextBoxWithValidation = (fieldRenderProps) => {
+  const { validationMessage, visited, ...others } = fieldRenderProps;
+  const anchor = useRef(null);
+
+  return (
+    <div>
+      <NumericTextBox {...others} ref={anchor} />
+      {visited && validationMessage && <Error>{validationMessage}</Error>}
+    </div>
+  );
+};
+
+export const NumberCell = (props) => {
+  const { parentField, editIndex } = useContext(FormGridEditContext);
+  const isInEdit = props.dataItem[FORM_DATA_INDEX] === editIndex;
+
+  return (
+    <td>
+      <Field
+        component={isInEdit ? NumericTextBoxWithValidation : DisplayValue}
+        name={`${parentField}[${props.dataItem[FORM_DATA_INDEX]}].${props.field}`}
+        validator={minValidator}
+      />
+    </td>
+  );
+};
+
+export const NameCell = (props) => {
+  const { parentField, editIndex } = useContext(FormGridEditContext);
+  const isInEdit = props.dataItem[FORM_DATA_INDEX] === editIndex;
+  
+  return (
+    <td>
+      <Field
+        component={isInEdit ? TextInputWithValidation : DisplayValue}
+        name={`${parentField}[${props.dataItem[FORM_DATA_INDEX]}].${props.field}`}
+        validator={requiredValidator}
+      />
+    </td>
+  );
+};
--- forms/deep-dive/src/components/fieldarray-component/index.jsx
+++ forms/deep-dive/src/components/fieldarray-component/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, createContext, useCallback } from 'react';
+import { useState, useRef, createContext, useContext, useCallback } from 'react';
 import { 
 	Form,
 	FormElement,
@@ -11,6 +11,8 @@ import {
 	GridColumn as Column,
 	GridToolbar 
 } from '@progress/kendo-react-grid';
+import { Button } from '@progress/kendo-react-buttons';
+import { Ripple } from '@progress/kendo-react-ripple';
 import { sampleProducts as products } from '../../data/sample-products';
 
 // Validate the entire Form
@@ -23,3 +25,105 @@ export const FormGridEditContext = createContext({});
 const FORM_DATA_INDEX = "formDataIndex";
 const DATA_ITEM_KEY = "ProductID";
 
+// Add a command cell to Edit, Update, Cancel, and Delete an item
+const CommandCell = (props) => {
+	const { 
+		onRemove, 
+		onEdit, 
+		onSave, 
+		onCancel, 
+		editIndex 
+	} = useContext(FormGridEditContext);
+	const isInEdit = props.dataItem[FORM_DATA_INDEX] === editIndex;
+	const isNewItem = !props.dataItem[DATA_ITEM_KEY];
+
+	const onRemoveClick = useCallback(
+		(e) => {
+			e.preventDefault();
+			onRemove(props.dataItem);
+		},
+		[props.dataItem, onRemove]
+	);
+
+	const onEditClick = useCallback(
+		(e) => {
+			e.preventDefault();
+			onEdit(props.dataItem, isNewItem);
+		},
+		[props.dataItem, onEdit, isNewItem]
+	);
+
+	const onSaveClick = useCallback(
+		(e) => {
+			e.preventDefault();
+			onSave();
+		},
+		[onSave]
+	);
+
+	const onCancelClick = useCallback(
+		(e) => {
+			e.preventDefault();
+			onCancel();
+		},
+		[onCancel]
+	);
+
+	return isInEdit ? (
+		<td className="k-command-cell">
+			<Button
+				className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base k-grid-save-command"
+				onClick={onSaveClick}
+			>
+				{isNewItem ? "Add" : "Update"}
+			</Button>
+			<Button
+				className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base k-grid-cancel-command"
+				onClick={isNewItem ? onRemoveClick : onCancelClick}
+			>
+				{isNewItem ? "Discard" : "Cancel"}
+			</Button>
+		</td>
+	) : (
+		<td className="k-command-cell">
+			<Button
+				className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-primary k-grid-edit-command"
+				onClick={onEditClick}
+			>
+				Edit
+			</Button>
+			<Button
+				className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base k-grid-remove-command"
+				onClick={onRemoveClick}
+			>
+				Remove
+			</Button>
+		</td>
+	);
+};
+
+// Create the Grid that will be used inside the Form
+const FormGrid = (fieldArrayRenderProps) => {
+	const { 
+		validationMessage, 
+		visited, 
+		name, 
+		dataItemKey 
+	} = fieldArrayRenderProps;
+	const [editIndex, setEditIndex] = useState();
+	const editItemCloneRef = useRef();
+
+	// Add a new item to the Form FieldArray that will be shown in the Grid
+	const onAdd = useCallback(
+		(e) => {
+			e.preventDefault();
+			fieldArrayRenderProps.onUnshift({
+				value: {
+					id: "",
+					name: ""
+				},
+			});
+		},
+		[fieldArrayRenderProps]
+	);
+};
